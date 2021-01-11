ffa1fe0ebab5429960022b8a4d1bf5cbcfa662a6
Pranav Prashar <prasherlucky@gmail.com>
2021-01-11 11:59:58 -0500

    Update File

--- pythonBot.py
+++ pythonBot.py
@@ -1,7 +1,8 @@
 import praw
 import tweepy
+import time
 
-# from credentials import *
+from credentials import *
 
 tw_auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
 tw_auth.set_access_token(twitter_access_token, twitter_access_token_secret)
@@ -11,6 +12,25 @@ tw_api = tweepy.API(tw_auth)
 my_reddit = praw.Reddit(client_id=reddit_client_id, client_secret=reddit_client_secret, user_agent=reddit_user_agent)
 sub_name = 'Coronavirus'
 max_posts = 10
+
+
+
+
 for submission in my_reddit.subreddit(sub_name).new(limit=max_posts):
-	print(submission.title)
+    	
+	# print(submission.title)
+	# # print(submission.id)
+	# print('Link to article' , str(submission.url))
+	# # print('Author',str(submission.author))
+	# print("\n")
+	output = submission.title 
+	output2 = "Link to article:" + str(submission.url)
+	outputFinal = submission.title + "\n" + str(submission.url) + "\n"
+	# print(output , '\n' , output2)
+	print(outputFinal)
+	tw_api.update_status(status=outputFinal) 
+
+	
+
+	
 
